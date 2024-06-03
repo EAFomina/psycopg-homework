13db9f6c973becf3ea4ea0a10a8513fca5a8b117
Ekaterina Fomina <eafomina2022@mail.ru>
2024-06-03 22:01:41 +0300

    functions changed

--- customer_data.py
+++ customer_data.py
@@ -39,9 +39,20 @@ def add_phone_number(conn, customer_id, phone_number):
 # Функция, позволяющая изменить данные о клиенте
 def change_data(conn, customer_id, first_name=None, last_name=None, email=None):
     with conn.cursor() as cur:
-        cur.execute("""
-        UPDATE customer SET first_name=%s, last_name=%s, email=%s WHERE customer_id=%s;
-        """, (first_name, last_name, email, customer_id))
+        if first_name: 
+            cur.execute("""
+            UPDATE customer SET first_name=%s WHERE customer_id=%s;
+            """, (first_name, customer_id))
+        
+        if last_name:
+            cur.execute("""
+            UPDATE customer SET last_name=%s WHERE customer_id=%s;
+            """, (last_name, customer_id))
+
+        if email:
+            cur.execute("""
+            UPDATE customer SET email=%s WHERE customer_id=%s;
+            """, (email, customer_id))
         conn.commit()
 
 # Функция, позволяющая удалить телефон для существующего клиента
@@ -61,52 +72,72 @@ def delete_customer(conn, customer_id):
         conn.commit()
 
 # Функция, позволяющая найти клиента по его данным: имени, фамилии, email или телефону
-def find_customer(conn, first_name=None, last_name=None, email=None, phone_number=None):
+def find_customer(conn, **info):
+    query = 'SELECT * FROM phone p JOIN customer c ON p.customer_id = c.customer_id WHERE 1=1 '
+    params = []
+
+    if 'first_name' in info:
+        query += 'AND first_name=%s '
+        params.append(info['first_name'])
+    
+    if 'last_name' in info:
+        query += 'AND last_name=%s '
+        params.append(info['last_name'])
+    
+    if 'email' in info:
+        query += 'AND email=%s '
+        params.append(info['email'])
+    
+    if 'phone_number' in info:
+        query += 'AND phone_number=%s '
+        params.append(info['phone_number'])
+
     with conn.cursor() as cur:
-        cur.execute("""
-        SELECT * FROM phone p 
-        JOIN customer c ON p.customer_id = c.customer_id
-        WHERE first_name=%s OR last_name=%s OR email=%s OR phone_number=%s;
-        """, (first_name, last_name, email, phone_number))
+        cur.execute(query, params)
         print(cur.fetchall())
 
 # Вызов функций 
-with psycopg2.connect(database='customer_db', user='postgres', password='postgres') as conn:
-    # Создание таблиц
-    create_table(conn)
+if __name__ == '__main__':
+    with psycopg2.connect(database='customer_db', user='postgres', password='postgres') as conn:
+        # Создание таблиц
+        create_table(conn)
     
-    # Добавление новых клиентов
-    add_customer(conn, 'Мария', 'Плотникова', 'MaryPlotnik@smth.com')
-    add_customer(conn, 'Дмитрий', 'Зубарев', 'Zuba@smth.com')
-    add_customer(conn, 'Максим', 'Минимумов', 'MaxMin@smth.com')
-    add_customer(conn, 'Нина', 'Иванова', 'IvaNina@smth.com')
-    add_customer(conn, 'Елизавета', 'Смирнова', 'ElizavetaSmirnova@smth.com')
-    add_customer(conn, 'Виктор', 'Кравец', 'ViktorKravec@smth.com')
+        # Добавление новых клиентов
+        add_customer(conn, 'Мария', 'Плотникова', 'MaryPlotnik@smth.com')
+        add_customer(conn, 'Дмитрий', 'Зубарев', 'Zuba@smth.com')
+        add_customer(conn, 'Максим', 'Минимумов', 'MaxMin@smth.com')
+        add_customer(conn, 'Нина', 'Иванова', 'IvaNina@smth.com')
+        add_customer(conn, 'Елизавета', 'Смирнова', 'ElizavetaSmirnova@smth.com')
+        add_customer(conn, 'Виктор', 'Кравец', 'ViktorKravec@smth.com')
+        add_customer(conn, 'Лиана', 'Крубееева', 'ViktoriyaKurbeeva@smth.com')
 
-    # Добавление телефонных номеров клиентов
-    add_phone_number(conn, 1, '9321436823')
-    add_phone_number(conn, 1, '9321427823')
-    add_phone_number(conn, 2, '9121436070')
-    add_phone_number(conn, 3, '9500013129')
-    add_phone_number(conn, 4, '9092788800')
-    add_phone_number(conn, 4, '9001368018')
-    add_phone_number(conn, 5, '9119283467')
-    add_phone_number(conn, 6, '9329009999')
-    add_phone_number(conn, 6, '9329209303')
+        # Добавление телефонных номеров клиентов
+        add_phone_number(conn, 1, '9321436823')
+        add_phone_number(conn, 1, '9321427823')
+        add_phone_number(conn, 2, '9121436070')
+        add_phone_number(conn, 3, '9500013129')
+        add_phone_number(conn, 4, '9092788800')
+        add_phone_number(conn, 4, '9001368018')
+        add_phone_number(conn, 5, '9119283467')
+        add_phone_number(conn, 6, '9329009999')
+        add_phone_number(conn, 6, '9329209303')
+        add_phone_number(conn, 7, '9028733718')
+        add_phone_number(conn, 7, '9022933700')
 
-    # Изменение информации о клиенте
-    change_data(conn, 4, 'Нина', 'Иванова', 'IvanovaNina@smth.com')
+        # Изменение информации о клиенте
+        change_data(conn, 7, last_name='Курбанова', email='LianaKurbanova@smth.com')
 
-    # Удаление телефонного номера клиента
-    delete_phone_number(conn, 4, '9001368018')
+        # Удаление телефонного номера клиента
+        delete_phone_number(conn, 4, '9001368018')
 
-    # # Удаление существующего клиента
-    delete_customer(conn, 1)
+        # Удаление существующего клиента
+        delete_customer(conn, 1)
 
-    # Поиск клиента по его данным
-    find_customer(conn, first_name='Виктор')
-    find_customer(conn, last_name='Зубарев')
-    find_customer(conn, email='ViktorKravec@smth.com')
-    find_customer(conn, phone_number='9321436823')
+        # Поиск клиента по его данным
+        find_customer(conn, first_name='Виктор', last_name='Кравец')
+        find_customer(conn, last_name='Зубарев')
+        find_customer(conn, email='ViktorKravec@smth.com')
+        find_customer(conn, phone_number='9321436823')
+        find_customer(conn, first_name='Лиана')
 
-conn.close()
+    conn.close()
